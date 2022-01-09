1fbcd9908d30cd12ee11de0929abadc47342c9a7
MelihOmer <97173932+MelihOmer@users.noreply.github.com>
2022-01-09 19:51:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımlama
+            string[] renkler = new string[5];
+            string[] hayvanlar ={"kedi","köpek","kuş","maymun"};
+            int[] dizi;
+            dizi =new int[5];
+
+            //Dizilere değer atama ve erişim
+            renkler[0] = "Mavi";
+            dizi[3] =10;
+            System.Console.WriteLine(dizi[3]);
+            System.Console.WriteLine(hayvanlar[1]);
+            System.Console.WriteLine(renkler[0]);
+
+            //Döngüler ile dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            System.Console.WriteLine("Lütfen Dizinin Eleman Sayısını Giriniz");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int [] sayiDizisi = new int[diziUzunlugu];
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                System.Console.WriteLine("Lütfen {0}. sayıyı giriniz", i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var item in sayiDizisi)
+            {
+                toplam += item;
+            }
+            System.Console.WriteLine("Ortalama : "+ toplam/diziUzunlugu);
+
+        }
+    }
+}
